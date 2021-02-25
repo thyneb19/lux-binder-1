3e3c1da21da1db10595980b7e512861fab0c4f9b
19thyneb <thyne.boonmark@gmail.com>
2021-02-25 11:09:44 -0800

    Update Postgresql Version Reference

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ PGDATA=${PGDATA:-/home/newuser/srv/pgsql}
 if [ ! -d "$PGDATA" ]; then
   /usr/lib/postgresql/12/bin/initdb -D "$PGDATA" --auth-host=md5 --encoding=UTF8
 fi
-/usr/lib/postgresql/12/bin/pg_ctl -D "$PGDATA" status || /usr/lib/postgresql/10/bin/pg_ctl -D "$PGDATA" -l "$PGDATA/pg.log" start
+/usr/lib/postgresql/12/bin/pg_ctl -D "$PGDATA" status || /usr/lib/postgresql/12/bin/pg_ctl -D "$PGDATA" -l "$PGDATA/pg.log" start
 
 psql postgres -c "CREATE USER test PASSWORD 'testpass'"
 createdb -O test test
